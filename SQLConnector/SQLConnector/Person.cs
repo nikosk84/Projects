f45fed20601b493501500f8f6526ed05aab807f4
nikosk84 <nikoswin84@outlook.com>
2018-04-23 15:57:49 +0300

    Updated SQLConnector

---
+++ SQLConnector/SQLConnector/Person.cs
@@ -0,0 +1,19 @@
+﻿namespace SQLConnector
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// Output string from search result
+        /// </summary>
+        public string FullPersonInfo
+        {
+            get
+            {
+                return $"{FirstName}, {LastName}";
+            }
+        }
+    }
+}
