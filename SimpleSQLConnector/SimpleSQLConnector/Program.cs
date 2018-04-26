c65352dad4e9690fa1264269db5397bd1881e15e
nikosk84 <nikoswin84@outlook.com>
2018-04-26 15:15:28 +0300

    Added stored procedure call

--- SimpleSQLConnector/SimpleSQLConnector/Program.cs
+++ SimpleSQLConnector/SimpleSQLConnector/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data;
 using System.Data.SqlClient;
 
 namespace SimpleSQLConnector
@@ -7,11 +8,34 @@ namespace SimpleSQLConnector
     {
         static void Main(string[] args)
         {
+            //Connect to database
             using (SqlConnection connection = new SqlConnection(GlobalConnection.Connection("SQLConnectorTester")))
             {
                 connection.Open();
-                Console.WriteLine($"Server Version: {connection.ServerVersion}");
-                Console.WriteLine($"State: {connection.State}");
+                Console.WriteLine($"Connection State: {connection.State}");
+                Console.WriteLine("Search by last name: ");
+
+                //Add command to retrieve from stored procedure passing in the connection
+                using (SqlCommand command = new SqlCommand("dbo.spPerson_GetByLastName", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.Add("@LastName", SqlDbType.VarChar).Value = Console.ReadLine();
+
+                    //Read fetched data and display it to the console
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            Console.WriteLine($"Record found: {reader.GetString(0)} {reader.GetString(1)}");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("No records found.");
+                    }
+                }
             }
             Console.ReadLine();
         }
